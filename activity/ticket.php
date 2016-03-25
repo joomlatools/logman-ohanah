3ae342b39d635bf1fec26e8a3275c6c4a42a9a0e
Arunas Mazeika <arunas@timble.net>
2016-03-25 13:59:47 +0100

    Renamed activity entity.

--- activity/ticket.php
+++ activity/ticket.php
@@ -7,28 +7,45 @@
  */
 
 /**
- * Attendee Activity Entity
+ * Ticket Activity Entity
  *
  * @author  Arunas Mazeika <https://github.com/amazeika>
  * @package Joomlatools\Plugin\LOGman
  */
-class PlgLogmanOhanahActivityTicket extends PlgLogmanOhanahActivityAttendee
+class PlgLogmanOhanahActivityTicket extends ComLogmanModelEntityActivity
 {
-    protected $_unassigned;
-
     protected function _initialize(KObjectConfig $config)
     {
-        // TODO: Improve check for unassigned tickets (metadata???).
-        if ($this->_unassigned = empty(trim($config->data->title))) {
-            $config->append(array('format' => '{actor} {action} {object.type} {target.subtype} {target} {target.type}'));
-        }
+        $config->append(array(
+            'format'        => '{actor} {action} {object.type} {target.subtype} {target} {target.type}',
+            'object_table'  => 'ohanah_tickets',
+            'object_column' => 'ohanah_ticket_id'
+        ));
 
         parent::_initialize($config);
     }
 
+    protected function _objectConfig(KObjectConfig $config)
+    {
+        $config->append(array(
+            'url' => 'option=com_ohanah&view=attendee&id=' . $this->row
+        ))->append(array('type' => array('find' => 'object', 'url' => $config->url)));
+
+        parent::_objectConfig($config);
+    }
+
+    protected function _actionConfig(KObjectConfig $config)
+    {
+        if ($this->verb == 'add') {
+            $config->append(array('objectName' => 'ordered'));
+        }
+
+        parent::_actionConfig($config);
+    }
+
     public function getPropertyImage()
     {
-        if ($this->verb == 'order') {
+        if ($this->verb == 'add') {
             $image = 'icon-shopping-cart';
         } else {
             $image = parent::getPropertyImage();
@@ -37,18 +54,50 @@ class PlgLogmanOhanahActivityTicket extends PlgLogmanOhanahActivityAttendee
         return $image;
     }
 
-    protected function _objectConfig(KObjectConfig $config)
+    public function getPropertyTarget()
     {
-        if ($this->_unassigned)
-        {
-            $config->append(array(
-                'type' => array(
-                    'find' => 'object',
-                    'url'  => sprintf('option=com_ohanah&view=attendee&id=%s', $this->row)
-                )
-            ));
+        $metadata = $this->getMetadata();
+
+        $event = $metadata->event;
+
+        return $this->_getObject(array(
+            'subtype'    => array('object' => true, 'objectName' => 'Ohanah'),
+            'type'       => array(
+                'object'     => true,
+                'objectName' => 'event'
+            ),
+            'objectName' => $metadata->event->title,
+            'find'       => 'target',
+            'url'        => sprintf('option=com_ohanah&view=event&id=%s', $event->id)
+        ));
+    }
+
+    protected function _findActivityObject()
+    {
+        $result = parent::_findActivityObject();
+
+        if ($result) {
+            $result = $this->_findActivityTarget(); // Tickets cannot be reached if the event no longer exists.
         }
 
-        parent::_objectConfig($config);
+        return $result;
+    }
+
+    protected function _findActivityTarget()
+    {
+        $query = $this->getObject('lib:database.query.select')
+                      ->columns('COUNT(*)')
+                      ->table('ohanah_events')
+                      ->where('ohanah_event_id = :value')
+                      ->bind(array('value' => $this->getMetadata()->event->id));
+
+        // Need to catch exceptions here as table may not longer exist.
+        try {
+            $result = $this->getTable()->getAdapter()->select($query, KDatabase::FETCH_FIELD);
+        } catch (Exception $e) {
+            $result = 0;
+        }
+
+        return $result;
     }
 }
